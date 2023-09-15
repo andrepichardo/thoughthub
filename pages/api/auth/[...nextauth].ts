479ffcb738f59155a51ec11cc893e019d869eafb
André Pichardo <andrep97@hotmail.com>
2023-09-15 16:32:27 -0400

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '../../../prisma/client';
 import type { Adapter } from 'next-auth/adapters';
 
-
 export const authOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   secret: process.env.AUTH_SECRET,
