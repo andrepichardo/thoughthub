3e29d066f4ae69938ed8e67b1173b99f093f5b70
André Pichardo <andrep97@hotmail.com>
2025-04-02 11:28:07 -0400

    update

--- prisma/client.tsx
+++ prisma/client.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   namespace NodeJS {
@@ -16,6 +16,6 @@ declare const global: CustomNodeJsGlobal;
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
